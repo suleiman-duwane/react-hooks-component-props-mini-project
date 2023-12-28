0de489819961edc2c5f3494f42bbf2187aaf7267
Abdiaziz2233 <zizoalhusyny73@gmail.com>
2023-12-28 15:46:55 +0300

    Done

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Article({title, date = "January 1, 1970", preview, minutes}) {
+
+    return (
+    <article>
+        <h3>{title}</h3>
+        <small>{date}</small>
+        <p>{preview}</p>
+        <CoffeeCupDisplay minutes={minutes} />
+    </article>
+    )
+}
+
+export default Article
